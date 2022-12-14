1bfdfdb073e84acd209bb75efcf31fa4e5515f2d
Cutreson <dinhhongsonpt@gmail.com>
2022-12-14 19:19:24 +0700

    MVVC Demo

--- Wpf_Basic/02_MVVM_Demo/SimpleClass.cs
+++ Wpf_Basic/02_MVVM_Demo/SimpleClass.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace _02_MVVM_Demo
 {
-    internal class SimpleClass
+    public class SimpleClass
     {
+        public int a { get; set; }
+        public int b { get; set; }
+        public int sum { get; set; }
+        /// <summary>
+        /// Khoi tao du lieu
+        /// </summary>
+        /// <returns></returns>
+        public List<SimpleClass> InitData()
+        {
+            List<SimpleClass> lstResult = new List<SimpleClass>();
+            lstResult.Add(new SimpleClass { a = 1, b = 2, sum = 3 });
+            lstResult.Add(new SimpleClass { a = 4, b = 7, sum = 11 });
+            return lstResult;
+        }
     }
 }
