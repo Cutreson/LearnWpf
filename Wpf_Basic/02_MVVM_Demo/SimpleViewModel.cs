a28e719aa115a5b27b2571a032284113b503a4b8
Cutreson <dinhhongsonpt@gmail.com>
2022-12-14 17:02:16 +0700

    MVVM Demo

---
+++ Wpf_Basic/02_MVVM_Demo/SimpleViewModel.cs
@@ -0,0 +1,88 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using System.Collections.ObjectModel;
+
+namespace _02_MVVM_Demo
+{
+    public class SimpleViewModel : BindableBase
+    {
+        #region Properties
+        private int _firstNumber;
+        public int FirstNumber
+        {
+            get
+            {
+                return _firstNumber;
+            }
+            set
+            {
+                SetProperty(ref _firstNumber, value);
+            }
+        }
+        private int _secondNumber;
+        public int SecondNumber
+        {
+            get
+            {
+                return _secondNumber;
+            }
+            set
+            {
+                SetProperty(ref _secondNumber, value);
+            }
+        }
+        private int _sum;
+        public int Sum
+        {
+            get
+            {
+                return _sum;
+            }
+            set
+            {
+                SetProperty(ref _sum, value);
+            }
+        }
+        /// <summary>
+        /// List Sum binding to DataGrid
+        /// </summary>
+        private ObservableCollection<SimpleClass> _lstSum;
+        public ObservableCollection<SimpleClass> lstSum
+        {
+            get
+            {
+                return _lstSum;
+            }
+            set
+            {
+                SetProperty(ref _lstSum, value);
+            }
+        }
+        #endregion
+
+        #region Command
+        private DelegateCommand SubmitCommand { get; set; }
+        private void OnSubmit()
+        {
+            Sum = FirstNumber + SecondNumber;
+            lstSum.Add(new SimpleClass { a = FirstNumber, b = SecondNumber, sum = Sum });
+        }
+        private bool CanSubmit()
+        {
+            if(FirstNumber != 0 && SecondNumber != 0)
+            {
+                return true;
+            }
+            return false;
+        }
+        #endregion
+
+        public SimpleViewModel()
+        {
+            this.SubmitCommand= new DelegateCommand(OnSubmit, CanSubmit);
+            //Khoi tao du lieu ban dau cho DataGrid
+            SimpleClass temp = new SimpleClass();
+            lstSum = new ObservableCollection<SimpleClass>(temp.InitData());
+        }
+    }
+}
